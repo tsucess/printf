59984ac7d2210d4bc4d92836dfa85ed59c9eb182
Aliyumusamkk <aliyumusakalakala@gmail.com>
2023-04-25 13:52:53 -0700

    main.h updated

--- main.h
+++ main.h
@@ -7,16 +7,14 @@
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
 
-/* FLAGS
- */
+/* FLAGS */
 #define F_MINUS 1
 #define F_PLUS 2
 #define F_ZERO 4
 #define F_HASH 8
 #define F_SPACE 16
 
-/* SIZES
- */
+/* SIZES*/
 #define S_LONG 2
 #define S_SHORT 1
 
@@ -38,4 +36,4 @@ struct fmt
 	char fmt;
 	int (*fn)(va_list, char[], int, int, int, int);
 };
-#endif 
+#endif
