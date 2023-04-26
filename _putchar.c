bf4da6395a2d33b88c8857779028d56ac9688b2a
tsucess <taofeeq.muhammad22@gmail.com>
2023-04-26 15:27:47 -0700

    removed unwanted files

--- _putchar.c
+++ _putchar.c
@@ -3,7 +3,9 @@
  * _putchar - function that replicate putchar in
  * standard input output library
  * @c: character parameter
- * Return: interger value
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
